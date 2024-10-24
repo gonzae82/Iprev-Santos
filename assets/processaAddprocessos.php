8544f5009141dab2f5eb7daa0db09088fbdbef8b
gonzae82 <eg@elido.com.br>
2024-10-24 01:13:38 -0300

    commit processos

--- assets/processaAddprocessos.php
+++ assets/processaAddprocessos.php
@@ -29,16 +29,20 @@ if (isset($_POST) && !empty($_POST)) {
     // Preparação e execução da consulta SQL para inserção na tabela "processos"
     $SQL_PROCESSO_ADD = "INSERT INTO processos(NUMERO_PROCESSO_DIGITAL, NOME_REQUERENTE, CPF_REQUERENTE, REG_REQUERENTE, PROCESSO_ADMINISTRATIVO, CITACAO_REQUERENTE, CALCULO_IR, DATA_ATUALIZACAO, VALOR_PRINCIPAL, VALOR_ATUALIZADO, JUROS_MORATORIOS, CUSTAS, HONORARIOS, HONORARIOS_PERCENTUAL, IPREV_VALOR, NOME_RESPONSAVEL, CARGO_RESPONSAVEL, DATA_RESPONSAVEL, CONCLUSOES, CAPEP_VALOR)
         VALUES ('$numero_processo_digital', '$nome_requerente', '$cpf_requerente', '$reg_requerente', '$processo_administrativo', '$citacao_requerente', '$calculo_ir', '$data_atualizacao', '$valor_principal', '$valor_atualizado', '$juros_moratorios', '$custas', '$honorarios', '$honorarios_percentual', '$iprev_valor', '$nome_responsavel', '$cargo_responsavel', '$data_responsavel', '$conclusoes', '$capep_valor')";
+        
+        if ($mysqli->query($SQL_PROCESSO_ADD) === TRUE) {
 
-    if ($mysqli->query($SQL_PROCESSO_ADD) === TRUE) {
-        echo "Informações cadastradas com sucesso na tabela 'processos'!";
-    } else {
-        echo "Erro ao cadastrar informações na tabela 'processos': " . $mysqli->error;
-    }
+            // Redirecionar para a página de listagem de processos
+            //echo "Usuário atualizado com sucesso.";
+            header('Location: ../dashboard.php?dir=views&file=processosList&msg=Processo alterado com sucesso.');
+        } else {
+            // Caso ocorra algum erro
+            echo "Erro: " . $SQSQL_PROCESSO_ADD . "<br>" . $mysqli->error;
+        }    
     
-} else {
-    echo "Nenhum dado foi recebido.";
-}
+        } else {
+            echo "Nenhum dado foi recebido.";
+        }
 
 // Fecha a conexão
 $mysqli->close();
